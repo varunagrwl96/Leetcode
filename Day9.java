c58bc65a4fff49f2ad8c3b041124cc638e35e4fb
varunagrwl96 <varun.agrwl96@gmail.com>
2020-04-13 21:50:50 +0530

    day 9 straight traversing the input

--- Day9.java
+++ Day9.java
@@ -1,4 +1,4 @@
-/* Solution O(N) time and O(1) Space */
+/* Solution O(N) time and O(1) Space Back traversal*/
 class Solution {
     public boolean backspaceCompare(String S, String T) {
         return getString(S).compareTo(getString(T))==0;
@@ -18,5 +18,32 @@ class Solution {
             }
         }
         return sAns;
+    }
+}
+---------------------------------------------------------------
+/*Straight traversing the input with O(1) space and O(N) time */
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        return computeString(S).compareTo(computeString(T))==0;
+    }
+    
+    public String computeString(String S){
+        String ans="";
+        int slength = S.length();
+        int word=0;
+        for(int i=0;i<slength;i++){
+            if(S.charAt(i)!='#'){
+                word++;
+                ans+=S.charAt(i);
+            }else{
+                if(word>0){
+                    ans = ans.substring(0,word-1);
+                    word--;
+                }else if(word==0){
+                    ans="";
+                }
+            }
+        }
+        return ans;
     }
 }
