7dc6624dc1fae5a856a1f025992aeca3b9ade015
varunagrwl96 <varun.agrwl96@gmail.com>
2020-05-07 19:54:40 +0530

    cousins of binary tree

---
+++ May-30-Day-Challenge/Day-7-Cousins-in-Binary-Tree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int xdepth=0,ydepth=0;
+    TreeNode xparent,yparent;
+    
+    public boolean isCousins(TreeNode root, int x, int y) {
+        
+        findDepth(root,x,y,0,null);
+        return (xdepth==ydepth && xparent!=yparent);
+    }
+    
+    public void findDepth(TreeNode root, int x,int y, int depth, TreeNode parent){
+        if(root==null){
+            return;
+        }
+        if(root.val == x){
+            xparent=parent;
+            xdepth=depth;
+        }else if(root.val == y){
+            yparent=parent;
+            ydepth=depth;
+        }
+        findDepth(root.left,x,y,depth+1,root);
+        findDepth(root.right,x,y,depth+1,root);
+        }
+    }
