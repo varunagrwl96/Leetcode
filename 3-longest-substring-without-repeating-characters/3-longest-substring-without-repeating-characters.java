96c9033a73304fbf8ff90c122025d2e3d6758892
Varun Agarwal <varun.agrwl96@gmail.com>
2022-01-14 15:16:15 -0500

    Time: 2 ms (99.82%), Space: 38.9 MB (89.95%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -6,18 +6,26 @@ class Solution {
         int max=0;
         int[] arr = new int[128];
         while(end<s.length()){
-            if(arr[s.charAt(end)]==0){
-                arr[s.charAt(end)]++;
-                end++;
-                max = Math.max(max,end-start);
-            }else{
-                while(s.charAt(start)!=s.charAt(end)){
-                    arr[s.charAt(start)]--;
-                    start++;
-                }
+            arr[s.charAt(end)]++;
+            while(arr[s.charAt(end)]>1){
                 arr[s.charAt(start)]--;
                 start++;
             }
+            max = Math.max(max,end-start+1);
+            end++;
+            
+            // if(arr[s.charAt(end)]==0){
+            //     arr[s.charAt(end)]++;
+            //     end++;
+            //     max = Math.max(max,end-start);
+            // }else{
+            //     while(s.charAt(start)!=s.charAt(end)){
+            //         arr[s.charAt(start)]--;
+            //         start++;
+            //     }
+            //     arr[s.charAt(start)]--;
+            //     start++;
+            // }
         }
         return max;
     }
