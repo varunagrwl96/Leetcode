cd7655eded54b202be5a20fc862e17013d7714cd
Varun Agarwal <varun.agrwl96@gmail.com>
2022-01-17 12:11:49 -0500

    Time: 78 ms (25.37%), Space: 67.6 MB (14.48%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,22 +1,23 @@
 class Solution {
     public int maxLength(List<String> arr) {
-        int max=0;
-        List<String> result = new ArrayList();
-        result.add("");
-        for(String str : arr){
-            for(int i=0;i<result.size();i++){
-                Set<Character> set = new HashSet();
-                String newStr = result.get(i)+str;
-                for(char c : newStr.toCharArray()){
-                    set.add(c);
-                }
-                if(set.size()!=newStr.length()){
-                    continue;
-                }
-                result.add(newStr);
-                max=Math.max(max,newStr.length());
-            }
-        }
-        return max;
+        // Use depth first search recursion through arr
+        // building from an initial empty string
+        return dfs(arr, 0, "");
+    }
+
+    private int dfs(List<String> arr, int pos, String res) {    
+        // Use a set to check res for duplicate characters
+        Set<Character> resSet = new HashSet<>();
+        for (char c : res.toCharArray())
+            resSet.add(c);
+        if (res.length() != resSet.size())
+            return 0;
+
+        // Recurse through each possible next option
+        // and find the best answer
+        int best = res.length();
+        for (int i = pos; i < arr.size(); i++)
+            best = Math.max(best, dfs(arr, i + 1, res + arr.get(i)));
+        return best;
     }
 }
