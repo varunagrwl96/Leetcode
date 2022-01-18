84599a9b6349200175d231ecb781179b2c228de3
Varun Agarwal <varun.agrwl96@gmail.com>
2022-01-17 19:55:13 -0500

    Time: 19 ms (47.01%), Space: 51.5 MB (42.93%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,25 +1,14 @@
 class Solution {
     public int minDeletions(String s) {
         int freq[] = new int[26];
-	    for (char c : s.toCharArray())
-		    freq[c - 'a']++;
-	    Arrays.sort(freq);
-        int prev=freq[25];
-        int ans=0;
-        for(int i=24;i>=0;i--){
-            if(freq[i]!=0){
-                if(prev==freq[i]){
-                    ans++;
-                    freq[i]--;
-                }
-                else if(prev<freq[i]){
-                    ans=ans+freq[i]-prev;
-                    if(prev!=0) ans++;
-                    freq[i]=Math.max(0,prev-1);
-                }
-                prev=freq[i];
-            }
+        for (char c : s.toCharArray())
+            freq[c - 'a']++;
+        Arrays.sort(freq);
+        int keep = freq[25], prev = keep;
+        for (int i = 24; i >= 0 && freq[i] != 0 && prev != 0; i--) {
+            prev = Math.min(freq[i], prev - 1);
+            keep += prev;
         }
-        return ans;
+        return s.length() - keep;
     }
 }
