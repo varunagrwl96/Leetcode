7bb3d339d2dcc09a460c30e263d69b8b1c229c41
Varun Agarwal <varun.agrwl96@gmail.com>
2022-08-09 22:19:39 -0700

    Time: 2 ms (93.36%), Space: 42.5 MB (38.17%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -1,24 +1,24 @@
 class Solution {
     public int longestPalindrome(String s) {
-        HashMap<Character,Integer> hashmap = new HashMap();
+        int[] count = new int[128];
+        // HashMap<Character,Integer> hashmap = new HashMap();
         for(char c : s.toCharArray()){
-            if(hashmap.containsKey(c)){
-                hashmap.put(c,hashmap.get(c)+1);
-            }else{
-                hashmap.put(c,1);
-            }
+            count[c]++;
+            // if(hashmap.containsKey(c)){
+            //     hashmap.put(c,hashmap.get(c)+1);
+            // }else{
+            //     hashmap.put(c,1);
+            // }
         }
         int ans=0;
-        boolean odd = false;
-        for(char c : hashmap.keySet()){
-            if(hashmap.get(c)%2==0){
-                ans= ans+hashmap.get(c);
-            }else if(hashmap.get(c)/2>=1){
-                ans= ans+(hashmap.get(c)/2)*2;
-                hashmap.put(c,1);
+        boolean odd=false;
+        for(int c : count){
+            if(c%2==0){
+                ans=ans+c;
+            }else if(c/2 >=1){
+                ans = ans + (c/2)*2;
                 odd=true;
-            }
-            else{
+            }else{
                 odd=true;
             }
         }
@@ -27,5 +27,23 @@ class Solution {
         }else{
             return ans;
         }
+        // boolean odd = false;
+        // for(char c : hashmap.keySet()){
+        //     if(hashmap.get(c)%2==0){
+        //         ans= ans+hashmap.get(c);
+        //     }else if(hashmap.get(c)/2>=1){
+        //         ans= ans+(hashmap.get(c)/2)*2;
+        //         hashmap.put(c,1);
+        //         odd=true;
+        //     }
+        //     else{
+        //         odd=true;
+        //     }
+        // }
+        // if(odd){
+        //     return ans+1;
+        // }else{
+        //     return ans;
+        // }
     }
 }
