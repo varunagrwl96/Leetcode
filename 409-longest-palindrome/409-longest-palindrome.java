ea602cd211e9095d3858f67d0f9f60718b2bf045
Varun Agarwal <varun.agrwl96@gmail.com>
2022-08-09 22:06:50 -0700

    Time: 6 ms (54.41%), Space: 40.5 MB (94.08%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        HashMap<Character,Integer> hashmap = new HashMap();
+        for(char c : s.toCharArray()){
+            if(hashmap.containsKey(c)){
+                hashmap.put(c,hashmap.get(c)+1);
+            }else{
+                hashmap.put(c,1);
+            }
+        }
+        int ans=0;
+        boolean odd = false;
+        for(char c : hashmap.keySet()){
+            if(hashmap.get(c)%2==0){
+                ans= ans+hashmap.get(c);
+            }else if(hashmap.get(c)/2>=1){
+                ans= ans+((int)(hashmap.get(c)/2))*2;
+                hashmap.put(c,1);
+                odd=true;
+            }
+            else{
+                odd=true;
+            }
+        }
+        if(odd){
+            return ans+1;
+        }else{
+            return ans;
+        }
+    }
+}
