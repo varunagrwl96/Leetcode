571efc524cb0a79441cd1e81e05c52d1c1130383
Varun Agarwal <varun.agrwl96@gmail.com>
2022-09-04 12:07:48 -0400

    Time: 10 ms (18.62%), Space: 50.7 MB (6.97%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,16 +10,29 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(p.val<root.val && q.val<root.val){
-            return lowestCommonAncestor(root.left,p,q);
+        
+        while(root!=null){
+            if(p.val>root.val && q.val>root.val){
+                root=root.right;
+            }else if(p.val<root.val && q.val<root.val){
+                root=root.left;
+            }else{
+                return root;
+            }
         }
-        else if(p.val>root.val && q.val>root.val){
-            return lowestCommonAncestor(root.right,p,q);
-        }else{
-            return root;
-        }
+        return null;
+        
+        
+        // if(p.val<root.val && q.val<root.val){
+        //     return lowestCommonAncestor(root.left,p,q);
+        // }
+        // else if(p.val>root.val && q.val>root.val){
+        //     return lowestCommonAncestor(root.right,p,q);
+        // }else{
+        //     return root;
+        // }
     }
 }
 
 //Time -> O(n)
-//Space -> O(n) recursive stack
+//Space -> O(n) recursive stack or O(1) iteratively
