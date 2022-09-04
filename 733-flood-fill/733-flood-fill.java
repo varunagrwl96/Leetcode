50763c5fdba1278d12e257f93f03a1ea6980a7b5
Varun Agarwal <varun.agrwl96@gmail.com>
2022-09-04 12:19:52 -0400

    Time: 1 ms (90.94%), Space: 48.3 MB (31.07%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int oldcolor=image[sr][sc];
+        int newcolor=color;
+        if(newcolor==oldcolor) return image;
+        color(image,sr,sc,oldcolor,newcolor);
+        return image;
+    }
+    
+    public void color(int[][] image, int sr, int sc,int oldcolor, int newcolor){
+        if(sr<0 || sr>=image.length || sc<0 || sc>=image[0].length || image[sr][sc]!=oldcolor){
+            return;
+        }
+        image[sr][sc]=newcolor;
+        color(image,sr+1,sc,oldcolor,newcolor);
+        color(image,sr-1,sc,oldcolor,newcolor);
+        color(image,sr,sc+1,oldcolor,newcolor);
+        color(image,sr,sc-1,oldcolor,newcolor);
+    }
+}
