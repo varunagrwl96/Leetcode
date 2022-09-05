d17c45e1e2ab1b2c7be9fd9f75acf3e577bee74f
Varun Agarwal <varun.agrwl96@gmail.com>
2022-09-05 11:49:34 -0400

    Time: 2 ms (7.37%), Space: 41.7 MB (7.44%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -4,10 +4,10 @@ class Solution {
         int[][] dp = new int[m+1][n+1];
         // dp[0][1]=1;
         // dp[1][0]=1;
-        for(int j=1;j<=n;j++){
+        for(int j=0;j<=n;j++){
             dp[0][j]=1;
         }
-        for(int i=1;i<=m;i++){
+        for(int i=0;i<=m;i++){
             dp[i][0]=1;
         }
         for(int i=1;i<m;i++){
