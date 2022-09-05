821c37b8c7c8fc21539143f78386df5fb51b8a3a
Varun Agarwal <varun.agrwl96@gmail.com>
2022-09-05 11:54:02 -0400

    Time: 0 ms (100.00%), Space: 40.9 MB (59.48%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -7,6 +7,9 @@ class Solution {
         for(int i=0;i<m;i++){
             dp[i][0]=1;
         }
+        // for(int[] arr : dp) {
+        //     Arrays.fill(arr, 1);
+        // }
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
