5df4cd5038f7896728bb0e0742d7cbbebce3838e
Varun Agarwal <varun.agrwl96@gmail.com>
2022-09-05 11:48:25 -0400

    Time: 1 ms (53.08%), Space: 41.2 MB (35.12%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -16,5 +16,15 @@ class Solution {
             }
         }
         return dp[m-1][n-1];
+        
+        // int[][] dp = new int[m][n];
+        // dp[0][1]=1;
+        // dp[1][0]=1;
+        // for(int i=1;i<m;i++){
+        //     for(int j=1;j<n;j++){
+        //         dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        //     }
+        // }
+        // return dp[m-1][n-1];
     }
 }
