f37a7176124bde155f2f309629813ad8bd4a7925
Varun Agarwal <varun.agrwl96@gmail.com>
2022-09-05 11:50:38 -0400

    Time: 1 ms (53.08%), Space: 41.1 MB (42.51%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -2,8 +2,6 @@ class Solution {
     public int uniquePaths(int m, int n) {
         if(m==1 && n==1) return 1;
         int[][] dp = new int[m][n];
-        // dp[0][1]=1;
-        // dp[1][0]=1;
         for(int j=0;j<n;j++){
             dp[0][j]=1;
         }
