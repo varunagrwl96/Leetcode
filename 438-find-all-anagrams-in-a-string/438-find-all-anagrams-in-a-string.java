3a74f9908d5302d668e2b6c9f7d997e44f6a9e4a
Varun Agarwal <varun.agrwl96@gmail.com>
2022-09-06 19:05:13 -0400

    Time: 46 ms (35.17%), Space: 48.7 MB (33.29%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,9 +1,41 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int slength = s.length();
+        // int slength = s.length();
+        // int plength = p.length();
+        // List<Integer> ans = new ArrayList();
+        // HashMap<Character,Integer> smap = new HashMap();
+        // HashMap<Character,Integer> pmap = new HashMap();
+        // for(char c : p.toCharArray()){
+        //     if(pmap.containsKey(c)){
+        //         pmap.put(c,pmap.get(c)+1);
+        //     }else{
+        //         pmap.put(c,1);
+        //     }
+        // }
+        // for(int i=0;i<slength;i++){
+        //     char curr = s.charAt(i);
+        //     if(smap.containsKey(curr)){
+        //         smap.put(curr,smap.get(curr)+1);
+        //     }else{
+        //         smap.put(curr,1);
+        //     }
+        //     if(i>=plength){
+        //         curr = s.charAt(i-plength);
+        //         if(smap.get(curr)>1){
+        //             smap.put(curr,smap.get(curr)-1);
+        //         }else{
+        //             smap.remove(curr);
+        //         }
+        //     }
+        //     if(smap.equals(pmap)){
+        //         ans.add(i-plength+1);
+        //     }
+        // }
+        // return ans;
+        
+        int slength=s.length();
         int plength = p.length();
         List<Integer> ans = new ArrayList();
-        HashMap<Character,Integer> smap = new HashMap();
         HashMap<Character,Integer> pmap = new HashMap();
         for(char c : p.toCharArray()){
             if(pmap.containsKey(c)){
@@ -12,24 +44,31 @@ class Solution {
                 pmap.put(c,1);
             }
         }
-        for(int i=0;i<slength;i++){
-            char curr = s.charAt(i);
-            if(smap.containsKey(curr)){
-                smap.put(curr,smap.get(curr)+1);
-            }else{
-                smap.put(curr,1);
-            }
-            if(i>=plength){
-                curr = s.charAt(i-plength);
-                if(smap.get(curr)>1){
-                    smap.put(curr,smap.get(curr)-1);
-                }else{
-                    smap.remove(curr);
+        int left=0, right=0;
+        int counter = pmap.size();
+        while(right<slength){
+            char curr = s.charAt(right);
+            if(pmap.containsKey(curr)){
+                pmap.put(curr,pmap.get(curr)-1);
+                if(pmap.get(curr)==0){
+                    counter--;
                 }
             }
-            if(smap.equals(pmap)){
-                ans.add(i-plength+1);
+            right++;
+            while(counter==0){
+                char curr2 = s.charAt(left);
+                if(pmap.containsKey(curr2)){
+                    pmap.put(curr2,pmap.get(curr2)+1);
+                    if(pmap.get(curr2)>0){
+                        counter++;   
+                    }
+                }
+                if(right-left==plength){
+                    ans.add(left);
+                }
+                left++;
             }
+
         }
         return ans;
     }
