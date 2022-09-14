e7e28456fb644fc3d067a8f5440981b524701f3b
Varun Agarwal <varun.agrwl96@gmail.com>
2022-09-14 18:07:16 -0400

    Time: 26 ms (7.16%), Space: 43.3 MB (22.42%) - LeetHub

---
+++ 299-bulls-and-cows/299-bulls-and-cows.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String getHint(String secret, String guess) {
+        int bulls=0;
+        int cows=0;
+        HashMap<Character,Integer> hashmap = new HashMap();
+        for(char c : secret.toCharArray()){
+            hashmap.put(c,hashmap.getOrDefault(c,0)+1);
+        }
+        for(int i=0;i<guess.length();i++){
+            char c= guess.charAt(i);
+            if(hashmap.containsKey(c)){
+                if(c==secret.charAt(i)){
+                    bulls++;
+                    if(hashmap.get(c)>1){
+                        hashmap.put(c,hashmap.get(c)-1);
+                    }else{
+                        hashmap.remove(c);
+                    }
+                // }else{
+                //     // cows++;
+                // }
+                // if(hashmap.get(c)>1){
+                //     hashmap.put(c,hashmap.get(c)-1);
+                // }else{
+                //     hashmap.remove(c);
+                // }
+                }
+            }
+        }
+        for(int i=0;i<guess.length();i++){
+            char c= guess.charAt(i);
+            if(hashmap.containsKey(c) && c!=secret.charAt(i)){
+                cows++;
+                if(hashmap.get(c)>1){
+                        hashmap.put(c,hashmap.get(c)-1);
+                }else{
+                    hashmap.remove(c);
+                }
+            }
+        }
+        return bulls+"A"+cows+"B";
+    }
+}
