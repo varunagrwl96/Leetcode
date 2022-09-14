3278ba47bb325fb2d949bc22bf1da59404e21003
Varun Agarwal <varun.agrwl96@gmail.com>
2022-09-14 18:50:57 -0400

    Time: 2 ms (70.34%), Space: 42.3 MB (41.27%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,25 +1,45 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        Stack<Character> stackS = new Stack();
-        Stack<Character> stackT = new Stack();
-        for(char c : s.toCharArray()){
-            if(c=='#'){
-                if(!stackS.isEmpty()){
-                    stackS.pop();
-                }
-            }else{
-                stackS.push(c);
-            }
-        }
-        for(char c : t.toCharArray()){
-            if(c=='#'){
-                if(!stackT.isEmpty()){
-                    stackT.pop();
-                }
-            }else{
-                stackT.push(c);
-            }
-        }
-        return stackS.equals(stackT);
+        return getString(s).equals(getString(t));
     }
+    
+    public String getString(String x){
+        String res="";
+        int skip=0;
+            for(int i=x.length()-1;i>=0;i--){
+                char c= x.charAt(i);
+                if(c=='#'){
+                    skip++;
+                }else{
+                    if(skip>0){
+                        skip--;
+                    }else{
+                        res+=Character.toString(c);
+                    }
+                }
+            }
+        return res;
+        }
+        // Stack<Character> stackS = new Stack();
+        // Stack<Character> stackT = new Stack();
+        // for(char c : s.toCharArray()){
+        //     if(c=='#'){
+        //         if(!stackS.isEmpty()){
+        //             stackS.pop();
+        //         }
+        //     }else{
+        //         stackS.push(c);
+        //     }
+        // }
+        // for(char c : t.toCharArray()){
+        //     if(c=='#'){
+        //         if(!stackT.isEmpty()){
+        //             stackT.pop();
+        //         }
+        //     }else{
+        //         stackT.push(c);
+        //     }
+        // }
+        // return stackS.equals(stackT);
+// }
 }
