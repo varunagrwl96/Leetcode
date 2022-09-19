31aad0b2eb4e1e877da08869f83c4d7e257a070c
Varun Agarwal <varun.agrwl96@gmail.com>
2022-09-19 12:12:17 -0400

    Time: 6 ms (96.31%), Space: 42.3 MB (97.05%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        // PriorityQueue<Pair<String,Integer>> pq = new PriorityQueue();
+        HashMap<String, Integer> hashmap = new HashMap();
+        for(String word : words){
+            hashmap.put(word,hashmap.getOrDefault(word,0)+1);
+            
+        }
+        List<String> candidates = new ArrayList<>(hashmap.keySet());
+        // Collections.reverseOrder(candidates);
+        Collections.sort(candidates, (i1,i2) -> hashmap.get(i1).equals(hashmap.get(i2))? i1.compareTo(i2) : hashmap.get(i2)-hashmap.get(i1));
+        // for(String x : candidates){
+        //     System.out.print(x);
+        // }
+        return candidates.subList(0,k);
+        // return null;
+    }
+}
