f8ef3f7eab81de88a40fbe25b0a65c26a523a3dd
Varun Agarwal <varun.agrwl96@gmail.com>
2022-09-26 10:50:01 -0400

    Time: 18 ms (61.85%), Space: 79.6 MB (30.10%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -10,21 +10,39 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        List<Integer> list = new ArrayList();
-        ListNode temp=head;
-        while(temp!=null){
-            list.add(temp.val);
-            temp=temp.next;
+        if(head==null||head.next==null) return head;
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left,right);
+    }
+    
+    ListNode merge(ListNode node1, ListNode node2){
+        ListNode dummy = new ListNode();
+        ListNode dummyhead = dummy;
+        while(node1!=null && node2!=null){
+            if(node1.val<node2.val){
+                dummy.next=node1;
+                dummy=dummy.next;
+                node1=node1.next;
+            }else{
+                dummy.next=node2;
+                dummy=dummy.next;
+                node2=node2.next;
+            }
         }
-        int[] input = list.stream().mapToInt(i->i).toArray();
-        Arrays.sort(input);
-        ListNode prev = new ListNode(-1);
-        ListNode anshead=prev;
-        for(int i=0;i<input.length;i++){
-            ListNode curr = new ListNode(input[i]);
-            prev.next=curr;
-            prev=prev.next;
+        dummy.next = (node1!=null)? node1 : node2;
+        return dummyhead.next;
+    }
+    
+    public ListNode getMid(ListNode head){
+        ListNode slow=null, fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=(slow==null)? head : slow.next;
+            fast=fast.next.next;
         }
-        return anshead.next;
+        ListNode mid = slow.next;
+        slow.next=null;
+        return mid;
     }
 }
