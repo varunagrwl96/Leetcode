9568879737d0bac684d5ab0e28311ab9267d2b58
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-01 17:42:49 -0400

    Time: 6 ms (31.86%), Space: 42.4 MB (33.25%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,57 +1,40 @@
 class Solution {
     public String decodeString(String s) {
+        
+        int n = s.length();
+        String ans="";
         Stack<Character> stack = new Stack();
-        StringBuilder sb = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-        for(int i=0;i<s.length();i++){
+        
+        for(int i=0;i<n;i++){
             char c = s.charAt(i);
             if(c!=']'){
                 stack.push(c);
             }else{
-                sb=new StringBuilder();
-                sb2=new StringBuilder();
+                //Get string
+                StringBuilder sb = new StringBuilder();
                 while(!stack.isEmpty() && Character.isAlphabetic(stack.peek())){
                     sb.insert(0,stack.pop());
                 }
-                stack.pop(); //remove ]
+                stack.pop(); //remove [                
+                    
+                //Get count
+                StringBuilder sb2 = new StringBuilder();
                 while(!stack.isEmpty() && Character.isDigit(stack.peek())){
-                    sb2.insert(0,stack.pop());
+                    sb2.insert(0,stack.pop());    
                 }
-                int times = Integer.parseInt(sb2.toString());
-                while(times>0){
-                    for(char ch : sb.toString().toCharArray()){
+                
+                //Create string
+                int count = Integer.parseInt(sb2.toString());
+                while(count>0){
+                    for(char ch : sb.toString().toCharArray())  
                         stack.push(ch);
-                    }
-                    times--;
+                    count--;
                 }
             }
         }
-        StringBuilder ans = new StringBuilder();
-        while(!stack.isEmpty()){
-            ans.insert(0,stack.pop());
-        }
-        return ans.toString();
-            
-        //     if(Character.isDigit(c)){
-        //         stack.push(String.valueOf(c));
-        //     }else if(c=='['){
-        //         currsb= new StringBuilder();
-        //     }else if(Character.isAlphabetic(c)){
-        //         currsb.append(c);
-        //     }else if(c==']'){
-        //         int times = Integer.parseInt(stack.pop());
-        //         while(times>0){
-        //             sb.append(currsb);
-        //             times--;
-        //         }
-        //         // stack.push(sb.toString());
-        //     }
-        // }
-        // StringBuilder ans = new StringBuilder();
-        // while(!stack.isEmpty()){
-        //     sb.append(stack.pop());
-        // }
-        // return sb.reverse().toString();
-        // return sb.toString();
+        StringBuilder retv = new StringBuilder();
+        while(!stack.isEmpty())
+            retv.insert(0, stack.pop());
+        return retv.toString();
     }
 }
