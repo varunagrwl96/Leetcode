b78d25bc6bd6a8167c2c25449ab618d0925e4781
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-01 17:14:36 -0400

    Time: 0 ms (100.00%), Space: 43.3 MB (12.17%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,8 +1,8 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n=matrix.length;
-        for(int i=0;i<=(n-1)/2;i++){
-            for(int j=0;j<n/2;j++){
+        for(int i=0;i<n/2;i++){
+            for(int j=i;j<n-i-1;j++){
                 int temp = matrix[i][j];
                 matrix[i][j]=matrix[n-1-j][i];
                 matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
