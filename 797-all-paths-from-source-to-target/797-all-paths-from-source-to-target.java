ff2f37806a95f3628d448d198f4ce0f7ba0c22b2
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-02 16:26:46 -0400

    Time: 3 ms (88.77%), Space: 55.4 MB (31.75%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -2,13 +2,7 @@ class Solution {
     List<List<Integer>> ans = new ArrayList();
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         
-        // HashMap<Integer,List<Integer>> hashmap = new HashMap();
-        // for(int i=0;i<graph.length;i++){
-        //     hashmap.put(i,new ArrayList())
-        // }
-        // LinkedList<Integer> currans = new LinkedList();
         List<Integer> currans = new ArrayList();
-        // currans.addLast(0);
         currans.add(0);
         paths(graph,0,graph.length-1,currans);
         return ans;
@@ -22,10 +16,8 @@ class Solution {
         }
         
         for(int each : graph[node]){
-            // currans.addLast(each);
             currans.add(each);
             paths(graph,each,end,currans);
-            // currans.removeLast();
             currans.remove(currans.size()-1);
         }
     }
