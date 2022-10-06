a4ec390e27a2f87a22d8c8341968f181cfb5148c
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-06 16:36:53 -0400

    Time: 1 ms (99.97%), Space: 50.3 MB (97.28%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,19 +1,44 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n=nums.length;
-        int[] arr = new int[n+1];
-        List<Integer> list = new ArrayList();
-        for(int i=0;i<n;i++){
-            if(nums[i]>0 && nums[i]<=n){
-                arr[nums[i]]++;
+        int n = nums.length;
+        int i=0;
+        while(i<n){
+            if(nums[i]>=1 && nums[i]<n && nums[i]!=nums[nums[i]-1]){
+                swap(nums,i,nums[i]-1);
+            }else{
+                i++;
             }
         }
-        int i=1;
-        for(i=1;i<=n;i++){
-            if(arr[i]==0){
-                return i;
+        i=0;
+        while(i<n){
+            if(nums[i]!=i+1){
+                return i+1;
+            }else{
+                i++;
             }
         }
-        return i;
+        return i+1;
     }
+    
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+        // int n=nums.length;
+        // int[] arr = new int[n+1];
+        // List<Integer> list = new ArrayList();
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]>0 && nums[i]<=n){
+        //         arr[nums[i]]++;
+        //     }
+        // }
+        // int i=1;
+        // for(i=1;i<=n;i++){
+        //     if(arr[i]==0){
+        //         return i;
+        //     }
+        // }
+        // return i;
+    // }
 }
