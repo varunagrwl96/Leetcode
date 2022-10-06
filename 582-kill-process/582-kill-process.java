077baf02cdc632c1fc62b204d6a03ba10df68152
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-06 14:44:00 -0400

    Time: 54 ms (63.83%), Space: 69.7 MB (71.97%) - LeetHub

---
+++ 582-kill-process/582-kill-process.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
+        HashMap<Integer,List<Integer>> hashmap = new HashMap();
+        for(int i=0;i<ppid.size();i++){
+            if(hashmap.containsKey(ppid.get(i))){
+                int parent = ppid.get(i);
+                List<Integer> children = hashmap.get(parent);
+                children.add(pid.get(i));
+                hashmap.put(parent,children);
+            }else{
+                List<Integer> children = new ArrayList();
+                children.add(pid.get(i));
+                hashmap.put(ppid.get(i),children);
+            }
+        }
+        List<Integer> ans = new ArrayList();
+        Queue<Integer> queue = new LinkedList();
+        queue.add(kill);
+        while(!queue.isEmpty()){
+            int curr = queue.poll();
+            ans.add(curr);
+            List<Integer> children = hashmap.get(curr);
+            if(children==null) continue;
+            for(int child : children){
+                queue.add(child);
+            }
+        }
+        return ans;
+    }
+}
+// 3 1,5
+// 0 3
+// 5 10
+
+//5,10
