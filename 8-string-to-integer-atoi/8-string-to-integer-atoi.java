1675aa92f8a832600cd2235f7e0b0190f1c63746
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-06 15:09:53 -0400

    Time: 2 ms (91.75%), Space: 43.8 MB (12.36%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int myAtoi(String s) {
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+        int i=0;
+        int ans=0;
+        int sign=1;
+        int length = s.length();
+        while(i<length && s.charAt(i)==' '){
+                i++;
+        }
+        if(i==length) return 0;
+        
+        if(s.charAt(i)=='-' || s.charAt(i)=='+'){
+            sign = s.charAt(i) == '+' ? 1 : -1;
+            i++;
+        }
+        
+        while(i<length){
+            int digit = s.charAt(i) - '0';
+            if(digit<0 || digit>9){
+                break;
+            }
+            
+            if(max/10 < ans || (max/10 == ans && max %10 < digit)){
+                return sign==1 ? max : min;
+            }
+            ans = ans*10 + digit;
+            i++;
+        }
+        return sign*ans;
+    }
+}
