d276235e989c696306c662c5bceaaf4567b637c9
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-15 19:53:54 -0400

    Time: 16 ms (17.13%), Space: 45.3 MB (51.85%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -4,10 +4,11 @@ class Solution {
         for(String word : words){
             hashmap.put(word,hashmap.getOrDefault(word,0)+1);
         }
-        
-        PriorityQueue<String> pq = new PriorityQueue<String>((i1,i2) -> (hashmap.get(i1) == hashmap.get(i2)) ? i2.compareTo(i1)  : hashmap.get(i1).compareTo(hashmap.get(i2)));
-        for(String key : hashmap.keySet()){
-            pq.add(key);
+        PriorityQueue<String> pq = new PriorityQueue<>(
+        (i1,i2) -> hashmap.get(i1).equals(hashmap.get(i2)) ? i2.compareTo(i1) : hashmap.get(i1).compareTo(hashmap.get(i2))
+        );
+        for(String word : hashmap.keySet()){
+            pq.add(word);
             if(pq.size()>k){
                 pq.poll();
             }
@@ -18,5 +19,19 @@ class Solution {
         }
         Collections.reverse(ans);
         return ans;
+        
+        // List<String> ans = new ArrayList();
+        // for(int i=0;i<k;i++){
+        //     ans.add(pq.poll());
+        // }
+        // return ans;
+        
+        // List<String> candidates = new ArrayList<>(hashmap.keySet());
+        // Collections.sort(candidates, (i1,i2) -> hashmap.get(i1).equals(hashmap.get(i2))? i1.compareTo(i2) : hashmap.get(i2).compareTo(hashmap.get(i1)));
+        // return candidates.subList(0,k);
     }
 }
+//Time O(Nlogk)
+//Space O(N)
+//Time O(NlogN)
+//Space O(N)
