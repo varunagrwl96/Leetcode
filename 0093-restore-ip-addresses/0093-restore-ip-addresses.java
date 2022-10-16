d410b8cc52491f302407dfda0998aed49d048fa6
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-16 11:56:16 -0400

    Time: 3 ms (92.09%), Space: 42.5 MB (88.29%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,27 +1,24 @@
 class Solution {
-    
     List<String> ans = new ArrayList();
-    LinkedList<String> segments = new LinkedList<String>();
+    String s=null;
     int n;
-    String s;
-    
+    LinkedList<String> segments = new LinkedList();
     public List<String> restoreIpAddresses(String s) {
-        n=s.length();
         this.s=s;
+        n=s.length();
         backtrack(-1,3);
         return ans;
     }
     
-    public void backtrack(int prev_pos, int dots){
-        int max_pos = Math.min(n-1,prev_pos+4);
-        for(int i=prev_pos+1;i<max_pos;i++){
-            String segment = s.substring(prev_pos+1,i+1);
-            if(valid(segment)){
+    public void backtrack(int prevpos, int dots){
+        int max_pos = Math.min(prevpos+4,n-1); 
+        for(int i=prevpos+1;i<max_pos;i++){
+            String segment = s.substring(prevpos+1,i+1);
+            if(isValid(segment)){
                 segments.add(segment);
                 if(dots-1==0){
                     update_ans(i);
-                }
-                else{
+                }else{
                     backtrack(i,dots-1);
                 }
                 segments.removeLast();
@@ -29,29 +26,20 @@ class Solution {
         }
     }
     
-    public boolean valid(String segment) {
-    /*
-    Check if the current segment is valid :
-    1. less or equal to 255      
-    2. the first character could be '0' 
-    only if the segment is equal to '0'
-    */
-    int m = segment.length();
-    if (m > 3)
-      return false;
-    return (segment.charAt(0) != '0') ? (Integer.valueOf(segment) <= 255) : (m == 1);
-  }
-    
-  public void update_ans(int curr_pos) {
-    /*
-    Append the current list of segments 
-    to the list of solutions
-    */
-    String segment = s.substring(curr_pos + 1, n);
-    if (valid(segment)) {
-      segments.add(segment);
-      ans.add(String.join(".", segments));
-      segments.removeLast();
+    public boolean isValid(String curr){
+        if(curr.length()>3){
+            return false;
+        }
+        return curr.charAt(0)=='0'? curr.length()==1 : Integer.valueOf(curr)<=255;
     }
-  }
+    
+    public void update_ans(int curr_pos){
+        String segment = s.substring(curr_pos+1,n);
+        if(isValid(segment)){
+            segments.add(segment);
+            ans.add(String.join(".",segments));
+            segments.removeLast();
+        }
+    }
+    
 }
