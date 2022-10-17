a1cc49b6d6a4361999a8a1135a8260f813d04549
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-17 16:00:03 -0400

    Time: 1 ms (86.53%), Space: 41.2 MB (53.30%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -2,19 +2,26 @@ public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         int ones=0;
-        // while(n!=0){
-            String x = Integer.toBinaryString(n);
-            for(char c : x.toCharArray()){
-                if(c=='1'){
-                    ones++;
-                }
-            }
+        while(n!=0){
+            // String x = Integer.toBinaryString(n);
+            // for(char c : x.toCharArray()){
+            //     if(c=='1'){
+            //         ones++;
+            //     }
+            // }
+            
             // int one = n%2;
-            // n=n>>1;
+            // n=n>>>1;
             // if(one==1){
             //     ones++;
             // }
-        // }
+            
+            int one = n&1;
+            n=n>>>1;
+            if(one==1){
+                ones++;
+            }
+        }
         return ones;
     }
 }
