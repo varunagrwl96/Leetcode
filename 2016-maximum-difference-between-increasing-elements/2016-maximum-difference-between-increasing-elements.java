f43f002279ef91e9bbdbff063b43aeda0a0f77dc
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-17 15:51:17 -0400

    Time: 1 ms (79.80%), Space: 42.7 MB (27.82%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.java
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.java
@@ -2,13 +2,13 @@ class Solution {
     public int maximumDifference(int[] nums) {
         int min = nums[0];
         int maxdiff = -1;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]<min){
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<=min){
                 min=nums[i];
             }else{
                 maxdiff=Math.max(maxdiff,nums[i]-min);
             }
         }
-        return maxdiff==0 ? -1 : maxdiff;
+        return maxdiff;
     }
 }
