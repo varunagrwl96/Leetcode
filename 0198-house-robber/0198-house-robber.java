1bce50b0c62ee7cf076c4a157ac7781022b66f6d
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-18 12:21:19 -0400

    Time: 0 ms (100.00%), Space: 41.4 MB (52.78%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        
+        int n = nums.length;
+        int[] dp  = new int[n+1];
+        dp[0]=0;
+        dp[1]=nums[0];
+        // dp[2]=nums[1];
+        for(int i=2;i<=n;i++){
+            dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i-1]);
+        }
+        return dp[n];
+    }
+}
+// 1 2 3 1
+// 0 1 2 
