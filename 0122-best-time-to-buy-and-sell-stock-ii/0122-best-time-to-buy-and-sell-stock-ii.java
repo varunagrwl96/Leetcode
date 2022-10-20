37df664a0505411f7a6c40eb701dc41ba875ca2a
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-20 14:59:27 -0400

    Time: 4 ms (27.00%), Space: 44.1 MB (57.25%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,18 +1,9 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        //Peak valley concept
-        int i = 0;
-        int valley = prices[0];
-        int peak = prices[0];
         int maxprofit = 0;
-        while (i < prices.length - 1) {
-            while (i < prices.length - 1 && prices[i] >= prices[i + 1])
-                i++;
-            valley = prices[i];
-            while (i < prices.length - 1 && prices[i] <= prices[i + 1])
-                i++;
-            peak = prices[i];
-            maxprofit += peak - valley;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
         }
         return maxprofit;
     }
