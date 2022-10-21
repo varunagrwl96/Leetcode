712504d6ccf9e5ed6b163d21d40d709c4e51bcc4
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-21 13:36:55 -0400

    Time: 0 ms (100.00%), Space: 41.1 MB (29.90%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,11 +1,30 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+        if(n<=2) return n;
+        int[] ways = new int[n+1];
+        ways[0]=0;
+        ways[1]=1;
+        ways[2]=2;
+        for(int i=3;i<=n;i++){
+            ways[i]=ways[i-1]+ways[i-2];
         }
-        return dp[n];
+        return ways[n];
     }
 }
+// Time O(n)
+// Space O(n)
+    
+    // public int climbStairs(int n) {
+    //     return climb_Stairs(0, n);
+    // }
+    // public int climb_Stairs(int i, int n) {
+    //     if (i > n) {
+    //         return 0;
+    //     }
+    //     if (i == n) {
+    //         return 1;
+    //     }
+    //     return climb_Stairs(i + 1, n) + climb_Stairs(i + 2, n);
+    // }
+//Time O(2^N)
+//Space O(n) recursion depth
