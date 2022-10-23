04c053f0673b4d8947b6e62d00a2d1963f8e1a71
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-23 19:34:45 -0400

    Time: 22 ms (25.00%), Space: 67.5 MB (27.07%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
@@ -1,35 +1,14 @@
 class Solution {
-    public int maxTurbulenceSize(int[] arr) {
-        //Question boils down to  counting valleys and mountains
-        //A valley is ex 3  1  4 ==> 1 is smaller than both left and right element
-    //A mountain is ex 5 8 4 ==> 8 is greater  than both right and left elemnt 
-        //To contribute in mountains or valleys we'll need three elements and then will make a check 
-        
-        if(arr.length<2)return arr.length;
-        int max = 1;
-        int start = 0;
-        int end = 0;
-        while(start<arr.length-1){
-            //if the two elements are equal it will help in make a skip
-            if(arr[start]==arr[start+1]){
-                start++;
-                continue;
-            }
-            end = start+1;
-            while(end+1<arr.length && isTur(arr,end)){
-                end++;
-            }
-            //Finding the max subarray and updating the ans
-            max = Math.max(max,end-start+1);
-            //reseting the start to current end
-            start = end;
+        public int maxTurbulenceSize(int[] A) {
+        int pre = 0, cur = 0, len = 1, res = 1; 
+        for (int i = 1; i < A.length; i++) {
+            cur = Integer.compare(A[i], A[i - 1]);
+            if (cur * pre == -1) len++;
+            else if (cur == 0) len = 1; 
+            else len = 2; 
+            res = Math.max(res, len); 
+            pre = cur; 
         }
-     return max;   
+        return res; 
     }
-    //Simple function to check for either valleys or mountains
-    
-    //return true if any of them is true
-    private boolean isTur(int[] arr, int k){
-            return(arr[k] > arr[k+1] && arr[k]>arr[k-1]) || (arr[k]<arr[k+1] && arr[k] < arr[k-1]);
-        }
 }
