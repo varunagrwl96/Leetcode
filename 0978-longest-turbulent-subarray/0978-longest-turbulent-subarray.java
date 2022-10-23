46511221ce4db2fafd873ff26707f452190fe863
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-23 19:35:03 -0400

    Time: 4 ms (100.00%), Space: 46.3 MB (97.18%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
@@ -1,5 +1,5 @@
 class Solution {
-        public int maxTurbulenceSize(int[] A) {
+    public int maxTurbulenceSize(int[] A) {
         int pre = 0, cur = 0, len = 1, res = 1; 
         for (int i = 1; i < A.length; i++) {
             cur = Integer.compare(A[i], A[i - 1]);
