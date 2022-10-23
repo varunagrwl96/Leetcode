0c7ab652f74588ea8e8bc75dec59286e8a037e09
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-22 23:25:56 -0400

    Time: 1 ms (88.63%), Space: 44.3 MB (8.84%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n<0){
+            x=1/x;
+            n=-n;
+        }
+        return recursivePow(x,n);
+    }
+    
+    public double recursivePow(double x, int N){
+        if(N==0) return 1; //base
+        if(N%2==0){
+            return recursivePow(x*x,N/2);
+        }else{
+            return x*recursivePow(x*x,N/2);
+        }
+    }
+}
+//O(logn)
+//O(1)
