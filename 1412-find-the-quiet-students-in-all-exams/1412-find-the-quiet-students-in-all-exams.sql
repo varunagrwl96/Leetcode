2163f91b0154dbf255f2af5b2935771657455632
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-29 13:33:07 -0400

    Time: 760 ms (71.43%), Space: 0B (100.00%) - LeetHub

--- 1412-find-the-quiet-students-in-all-exams/1412-find-the-quiet-students-in-all-exams.sql
+++ 1412-find-the-quiet-students-in-all-exams/1412-find-the-quiet-students-in-all-exams.sql
@@ -1,13 +1,11 @@
-/* Write your T-SQL query statement below */
-
-
-WITH cte AS(
-    SELECT exam_id, exam.student_id, student_name, score, RANK() OVER(PARTITION BY exam_id ORDER BY score) rk1, RANK() OVER(PARTITION BY exam_id ORDER BY score DESC) rk2 
-    FROM exam LEFT JOIN student
-    ON exam.student_id = student.student_id
+WITH cte AS 
+(
+	SELECT exam_id, student_id, 
+	RANK() OVER(partition by exam_id order by score DESC) AS "high_score",
+	RANK() OVER(partition by exam_id order by score) AS "low_score"
+	FROM Exam 
 )
 
-SELECT DISTINCT student_id, student_name
-FROM cte
-WHERE student_id NOT IN (SELECT student_id FROM cte WHERE rk1 = 1 or rk2 = 1)
-ORDER BY student_id
+SELECT DISTINCT e.student_id, s.student_name
+FROM Exam e LEFT JOIN Student s ON s.student_id = e.student_id
+WHERE e.student_id NOT IN (SELECT student_id FROM cte WHERE high_score = 1 OR low_score = 1)
