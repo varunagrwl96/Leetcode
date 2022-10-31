ff7d062dd7edf9f32937c90eeb970cd14c41026b
Varun Agarwal <varun.agrwl96@gmail.com>
2022-10-30 22:16:21 -0400

    Time: 1 ms (99.24%), Space: 40.7 MB (95.94%) - LeetHub

--- 0161-one-edit-distance/0161-one-edit-distance.java
+++ 0161-one-edit-distance/0161-one-edit-distance.java
@@ -1,22 +1,35 @@
 class Solution {
     public boolean isOneEditDistance(String s, String t) {
-        int ns = s.length();
-        int nt = t.length();
-        int i=0,j=0;
-        while(i<ns && j<nt && s.charAt(i) == t.charAt(j)){
+        
+        int l1 = s.length();
+        int l2 = t.length();
+        
+        if (Math.abs(l1 - l2) > 1) {
+            // more than 1 edit distance apart
+            return false;
+        }
+        
+        int i = 0;
+        int j = 0;
+        
+        while (i < l1 && j < l2 && s.charAt(i) == t.charAt(j)) {
             i++;
             j++;
         }
-        if(i==ns && j==nt){
+        
+        if (i == l1 && j == l2) {
             return false;
         }
-        if(ns==1 && nt==1) return true;
-        if(ns>nt){
-            return s.substring(i+1).equals(t.substring(j));
-        }else if(nt>ns){
-            return s.substring(i).equals(t.substring(j+1));
-        }else{
-            return s.substring(i+1).equals(t.substring(j+1));
+        
+        if (l1 > l2) {
+            // deletion is only possible
+            return s.substring(i + 1).equals(t.substring(j));
+        } else if (l1 < l2) {
+            // insertion is only possible
+            return s.substring(i).equals(t.substring(j + 1));
+        } else {
+            // replacing is only possible
+            return s.substring(i + 1).equals(t.substring(j + 1));
         }
     }
 }
