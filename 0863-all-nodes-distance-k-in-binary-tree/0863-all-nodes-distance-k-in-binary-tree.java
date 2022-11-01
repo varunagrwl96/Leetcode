355682239a81aaf676370d8f01c49368789b9975
Varun Agarwal <varun.agrwl96@gmail.com>
2022-11-01 14:52:05 -0400

    Time: 12 ms (90.27%), Space: 42.5 MB (81.22%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -9,40 +9,54 @@
  */
 class Solution {
     
-    Map<TreeNode, Integer> map = new HashMap<>();
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        HashMap<TreeNode, List<TreeNode>> hashmap = new HashMap();
+        HashSet<TreeNode> visitedSet = new HashSet();
+        buildMap(hashmap, root, null);
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(target);
+        visitedSet.add(target);
+        while(!queue.isEmpty()){
+            if(k==0){
+                break;
+            }
+            int size=queue.size();
+            
+            for(int i=0;i<size;i++){
+                TreeNode curr = queue.poll();
+                for(TreeNode nei : hashmap.get(curr)){
+                    if(visitedSet.contains(nei)) continue;
+                    queue.add(nei);
+                    visitedSet.add(nei);
+                }
+            }
+            k--;
+        }
+        List<Integer> ans = new ArrayList();
+        while(!queue.isEmpty()){
+            ans.add(queue.poll().val);
+        }
+        return ans;
+    }
+    
+    
+    public void buildMap(HashMap<TreeNode, List<TreeNode>> hashmap, TreeNode root, TreeNode parent){
+        if(root==null) return;
+        if(!hashmap.containsKey(root)){
+            hashmap.put(root,new 
+                        ArrayList<TreeNode>());
+        }
+        if(root.left!=null){
+            hashmap.get(root).add(root.left);
+        }
+        if(root.right!=null){
+            hashmap.get(root).add(root.right);
+        }
+        if(parent!=null){
+            hashmap.get(root).add(parent);
+        }
+        buildMap(hashmap, root.left, root);
+        buildMap(hashmap, root.right, root);
         
-    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
-        List<Integer> res = new LinkedList<>();
-        find(root, target);
-        dfs(root, target, K, map.get(root), res);
-        return res;
-    }
-    
-    // find target node first and store the distance in that path that we could use it later directly
-    private int find(TreeNode root, TreeNode target) {
-        if (root == null) return -1;
-        if (root == target) {
-            map.put(root, 0);
-            return 0;
-        }
-        int left = find(root.left, target);
-        if (left >= 0) {
-            map.put(root, left + 1);
-            return left + 1;
-        }
-		int right = find(root.right, target);
-		if (right >= 0) {
-            map.put(root, right + 1);
-            return right + 1;
-		}
-        return -1;
-    }
-    
-    private void dfs(TreeNode root, TreeNode target, int K, int length, List<Integer> res) {
-        if (root == null) return;
-        if (map.containsKey(root)) length = map.get(root);
-        if (length == K) res.add(root.val);
-        dfs(root.left, target, K, length + 1, res);
-        dfs(root.right, target, K, length + 1, res);
     }
 }
