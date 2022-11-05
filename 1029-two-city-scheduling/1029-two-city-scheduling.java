181403e48cc1ec43e9d69795499a26642ad0f90a
Varun Agarwal <varun.agrwl96@gmail.com>
2022-11-05 14:38:53 -0400

    Time: 3 ms (72.70%), Space: 42 MB (66.62%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -0,0 +1,24 @@
+class Solution {
+  public int twoCitySchedCost(int[][] costs) {
+//     Sort the persons in the ascending order by price_A - price_B parameter, which indicates the company additional costs.
+
+// To minimise the costs, send n persons with the smallest price_A - price_B to the city A, and the others to the city B.
+      
+    Arrays.sort(costs, (i1,i2) -> (i1[0]-i1[1]) - (i2[0]-i2[1]));
+
+    int total = 0;
+    int n = costs.length / 2;
+    // To optimize the company expenses,
+    // send the first n persons to the city A
+    // and the others to the city B
+    // for (int i = 0; i < n; ++i) total += costs[i][0] + costs[i + n][1];
+      for(int i=0;i<costs.length;i++){
+          if(i<n){
+           total+=costs[i][0];   
+          }else{
+          total+=costs[i][1];
+          }
+      }
+    return total;
+  }
+}
