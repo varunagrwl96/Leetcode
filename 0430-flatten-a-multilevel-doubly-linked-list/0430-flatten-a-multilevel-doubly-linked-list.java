53bceeaa1507a7a394f447d8ef6c2df804acdae7
Varun Agarwal <varun.agrwl96@gmail.com>
2022-11-05 13:50:09 -0400

    Time: 0 ms (100.00%), Space: 41.7 MB (75.98%) - LeetHub

---
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+
+    public Node() {}
+
+    public Node(int _val,Node _prev,Node _next,Node _child) {
+        val = _val;
+        prev = _prev;
+        next = _next;
+        child = _child;
+    }
+};
+*/
+class Solution {
+  public Node flatten(Node head) {
+    if (head == null) return head;
+    // pseudo head to ensure the `prev` pointer is never none
+    Node dummy = new Node(0, null, head, null);
+
+    flattenDFS(dummy, head);
+
+    // detach the pseudo head from the real head
+    dummy.next.prev = null;
+    return dummy.next;
+  }
+  /* return the tail of the flatten list */
+  public Node flattenDFS(Node prev, Node curr) {
+    if (curr == null) return prev;
+    curr.prev = prev;
+    prev.next = curr;
+
+    // the curr.next would be tempered in the recursive function
+    Node tempNext = curr.next;
+
+    Node tail = flattenDFS(curr, curr.child);
+    curr.child = null;
+
+    return flattenDFS(tail, tempNext);
+  }
+}
