a55b9b350a7440dde48052c3e7ed307c682c00d2
Varun Agarwal <varun.agrwl96@gmail.com>
2022-11-06 18:45:57 -0500

    Time: 59 ms (64.22%), Space: 107.8 MB (67.89%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,23 +1,51 @@
 class RandomizedSet {
     
-    HashSet<Integer> hashset;
+    HashMap<Integer,Integer> hashmap;
     Random r;
+    List<Integer> list;
     public RandomizedSet() {
-        hashset = new HashSet();
+        hashmap = new HashMap();
         r = new Random();
+        list=new ArrayList();
     }
     
     public boolean insert(int val) {
-        return hashset.add(val);
+        if(hashmap.containsKey(val)) return false;
+        list.add(val);
+        hashmap.put(val,list.size()-1);
+        return true;
     }
     
     public boolean remove(int val) {
-        return hashset.remove(val);
+        
+        //list remove with value/index is O(n) coz all elements in the right needs to be shifted
+        //So swap the last value with curr val and remove the last element
+        //Need index of curr removing element, thus need hashmap instead of hashset
+        if(!hashmap.containsKey(val)) return false;
+        int index = hashmap.get(val);
+        //Swap
+        // Collections.swap(list,index,list.size()-1);
+        // int swappedWith = list.get(index);
+        int last = list.get(list.size()-1);
+        list.set(index,last);
+        hashmap.put(last,index); //Update hashmap index for last ele
+        list.remove(list.size()-1);
+        hashmap.remove(val);
+        return true;
+        
+        
+        // int ind = valToInd.getOrDefault(val,-1);
+        // if(ind == -1) return false;
+        // Collections.swap(list,ind,list.size()-1);
+        // int swappedWith = list.get(ind);
+        // valToInd.put(swappedWith,ind);
+        // list.remove(list.size()-1);
+        // valToInd.remove(val);
+        // return true;
     }
     
     public int getRandom() {
-        int index = r.nextInt(hashset.size());
-        List<Integer> list = new ArrayList(hashset);
+        int index = r.nextInt(list.size());
         return list.get(index);
     }
 }
