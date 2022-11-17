bd087231ef321511315745e32456ebfb11ec19c7
Varun Agarwal <varun.agrwl96@gmail.com>
2022-11-17 11:09:11 -0500

    Time: 6 ms (17.88%), Space: 41.9 MB (63.94%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        int n = palindrome.length();
+        if(n==1) return "";
+        String ans="";
+        for(int i=0;i<(n/2);i++){
+            char curr = palindrome.charAt(i);
+            if(curr=='a'){
+                continue;
+            }else{
+                // System.out.println()
+                ans=palindrome.substring(0,i)+"a"+palindrome.substring(i+1);
+                return ans;
+            }
+        }
+        return palindrome.substring(0,n-1)+"b";
+    }
+}
