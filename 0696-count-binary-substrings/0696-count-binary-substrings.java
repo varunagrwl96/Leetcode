3cc6b1bd086d4fd44ab8e127a9240f8e3d320bf8
Varun Agarwal <varun.agrwl96@gmail.com>
2022-11-16 20:59:57 -0500

    Time: 16 ms (69.69%), Space: 52.6 MB (9.92%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.java
+++ 0696-count-binary-substrings/0696-count-binary-substrings.java
@@ -1,18 +1,35 @@
 class Solution {
     
     public int countBinarySubstrings(String s) {
-        int prevrunlength = 0;
-        int currunlength=1;
-        int ans=0;
-        for(int i=1;i<s.length();i++){
-            if(s.charAt(i)==s.charAt(i-1)){
-                currunlength++;
-            }else{
-                prevrunlength=currunlength;
-                currunlength=1;
+                int[] groups = new int[s.length()];
+        int t = 0;
+        groups[0] = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i-1) != s.charAt(i)) {
+                groups[++t] = 1;
+            } else {
+                groups[t]++;
             }
-            if(prevrunlength>=currunlength) ans++;
+        }
+
+        int ans = 0;
+        for (int i = 1; i <= t; i++) {
+            ans += Math.min(groups[i-1], groups[i]);
         }
         return ans;
+        
+        // int prevrunlength = 0;
+        // int currunlength=1;
+        // int ans=0;
+        // for(int i=1;i<s.length();i++){
+        //     if(s.charAt(i)==s.charAt(i-1)){
+        //         currunlength++;
+        //     }else{
+        //         prevrunlength=currunlength;
+        //         currunlength=1;
+        //     }
+        //     if(prevrunlength>=currunlength) ans++;
+        // }
+        // return ans;
     }
 }
