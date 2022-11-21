f2805d7a6e214822b96ad46b0586c7792a11589f
Varun Agarwal <varun.agrwl96@gmail.com>
2022-11-20 19:53:18 -0500

    Time: 17 ms (93.14%), Space: 41.6 MB (97.10%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,24 +1,14 @@
 class Solution {
-    // int min = Integer.MAX_VALUE;
-    Integer[] memo;
     public int coinChange(int[] coins, int amount) {
-        memo = new Integer[amount+1];
-        return recursion(coins,amount);
-    }
-    
-    
-    public int recursion(int[] coins, int remaining){
-        if(remaining==0) return 0;
-        if(remaining<0) return -1;
-        int min = Integer.MAX_VALUE;
-        if(memo[remaining]!=null) return memo[remaining];
-        for(int coin : coins){
-            int count = recursion(coins,remaining-coin);
-            if(count>=0){
-                min=Math.min(min,count+1);
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int coin : coins){
+                if(i-coin<0) continue;
+                dp[i]=Math.min(dp[i-coin]+1,dp[i]);
             }
         }
-        memo[remaining]= min==Integer.MAX_VALUE?-1:min;
-        return memo[remaining];
+        return dp[amount]==amount+1? -1 : dp[amount];
     }
 }
