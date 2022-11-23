41ad0daa1257baee5b22b87d7cdb9d68818cd383
Varun Agarwal <varun.agrwl96@gmail.com>
2022-11-23 13:03:39 -0500

    Time: 563 ms (5.02%), Space: 147.1 MB (9.86%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -1,71 +1,40 @@
-// class Solution {
-//     public List<List<Integer>> getAncestors(int n, int[][] edges) {
-//         HashMap<Integer,List<Integer>> hashmap = new HashMap();
-        
-//         for(int i=0;i<n;i++){
-//             hashmap.put(i,new ArrayList());
-//         }
-        
-//         for(int[] edge : edges){
-//             int src=edge[0];
-//             int dest=edge[1];
-//             hashmap.get(dest).add(src);
-//         }
-        
-//         List<List<Integer>> ans = new ArrayList();
-//         for(int i=0;i<n;i++){
-//             HashSet<Integer> currans = new HashSet();
-//             bfs(i,currans,hashmap);
-//             List<Integer> anslist =new ArrayList<Integer>(currans); 
-//             Collections.sort(anslist);
-//             ans.add(anslist);
-//         }
-//         return ans;
-//     }
-    
-//     public void bfs(int node, HashSet<Integer> currans, HashMap<Integer,List<Integer>> hashmap){
-//         List<Integer> parents = hashmap.get(node);
-//         if(parents.size()==0) return ;
-//         for(int parent : parents){
-//             currans.add(parent);
-//             bfs(parent,currans,hashmap);
-//         }
-//     }
-// }
 class Solution {
     public List<List<Integer>> getAncestors(int n, int[][] edges) {
-        List<Integer>[] parents = new ArrayList[n];
-        TreeSet<Integer>[] ancestors = new TreeSet[n];
-        for (int i = 0; i < n; ++ i) {
-            parents[i] = new ArrayList<>();
+        
+        HashMap<Integer,List<Integer>> hashmap = new HashMap();
+        int[] indegree = new int[n];
+        for(int i=0;i<n;i++){
+            hashmap.put(i,new ArrayList());
         }
         
-        for (int[] edge: edges) {
-            parents[edge[1]].add(edge[0]);
+        for(int[] edge : edges){
+            hashmap.get(edge[0]).add(edge[1]);
+            indegree[edge[1]]++;
         }
-        
-        List<List<Integer>> result = new ArrayList<>();
-        for (int i = 0; i < n; ++ i) {
-            if (ancestors[i] == null) {
-                dfs(i, parents, ancestors);
+        Queue<Integer> queue = new LinkedList();
+        List<Set<Integer>> parentsets = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            parentsets.add(new HashSet());
+            if(indegree[i]==0){
+                queue.add(i);
             }
-            
-            result.add(new ArrayList<>(ancestors[i]));
         }
         
-        return result;
-    }
-    
-    public void dfs(int node, List<Integer>[] parents, TreeSet<Integer>[] ancestors) {
-        TreeSet<Integer> set = new TreeSet<>();
-        for (int parent: parents[node]) {
-            set.add(parent);
-            if (ancestors[parent] == null) {
-                dfs(parent, parents, ancestors);
+        while(!queue.isEmpty()){
+            int parent = queue.poll();
+            List<Integer> children = hashmap.get(parent);
+            for(int child : children){
+                parentsets.get(child).add(parent);
+                parentsets.get(child).addAll(parentsets.get(parent));
+                indegree[child]--;
+                if(indegree[child]==0){
+                    queue.add(child);
+                }
             }
-            set.addAll(ancestors[parent]);
         }
-        
-        ancestors[node] = set;
+        List<List<Integer>> ans = new ArrayList<>();
+        for (Set<Integer> set : parentsets)
+            ans.add(new ArrayList<>(new TreeSet<>(set)));
+        return ans;
     }
 }
